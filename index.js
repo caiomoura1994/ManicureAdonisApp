200093a7c1d5144fcc49a82d3d2c4f1ec634a011
Caio Moura <caiomoura1994@gmail.com>
2018-09-26 12:03:06 -0300

    update third-party

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { AppRegistry } from 'react-native';
 import StorybookUI from './storybook';
 import App from './app/App';
 
-AppRegistry.registerComponent('AdonisApp', () => (__DEV__ ? StorybookUI : App));
+AppRegistry.registerComponent('AdonisApp', () => App);
+// AppRegistry.registerComponent('AdonisApp', () => (__DEV__ ? StorybookUI : App));
