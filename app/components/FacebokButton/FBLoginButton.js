537c386f41775f18a676745e9b6fbdabb3a02335
Caio Moura <caiomoura1994@gmail.com>
2018-10-01 09:47:21 -0300

    finish LoginWithFacebook

--- app/components/FacebokButton/FBLoginButton.js
+++ app/components/FacebokButton/FBLoginButton.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-import { View, Alert } from 'react-native';
-import { LoginButton } from 'react-native-fbsdk';
+import { View } from 'react-native';
+import { LoginButton, LoginManager, AccessToken } from 'react-native-fbsdk';
 
 export default class FBLoginButton extends Component {
+  facebookAuth() {
+    LoginManager.logInWithReadPermissions(['public_profile']).then(result => {
+      console.log(result);
+      AccessToken.getCurrentAccessToken().then((data) => {
+        const { accessToken } = data;
+        this.initUser(accessToken);
+      })
+    })
+  }
+  initUser(token) {
+    console.log(token);
+    // https://developers.facebook.com/docs/graph-api/reference/v2.2/user
+    fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((ERR) => {
+        console.log(ERR);
+      })
+  }
   render() {
     return (
       <View>
         <LoginButton
-          publishPermissions={['email']}
           onLoginFinished={
             (error, result) => {
               if (error) {
-                Alert.alert('Facebook', `Login failed with error: ${error.message}`);
+                alert("login has error: " + result.error);
               } else if (result.isCancelled) {
-                Alert.alert('Facebook', 'Login was cancelled');
+                alert("login is cancelled by user.");
               } else {
-                Alert.alert('Facebook', `Login was successful with permissions: ${result.grantedPermissions}`);
+                AccessToken.getCurrentAccessToken().then((data) => {
+                  const { accessToken } = data;
+                  this.initUser(accessToken);
+                }
+                )
               }
             }
           }
-          onLogoutFinished={() => Alert.alert('Facebook', 'User logged out')}
-        />
+          onLogoutFinished={() => alert('Logged out')} />
       </View>
     );
   }
